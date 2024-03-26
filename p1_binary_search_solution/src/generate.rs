0f9481d80e3d50f83810d2b460ad8bfecf98c0e1
christianteohx <christeo2001@hotmail.com>
2024-03-26 13:27:20 -0400

    Modified generate.rs and tests.rs for this homework

--- p1_binary_search_solution/src/generate.rs
+++ p1_binary_search_solution/src/generate.rs
@@ -1,3 +1,4 @@
+use std::array;
 use std::env::args;
 use std::mem::{transmute, size_of};
 use std::{ops::Range, fs::File};
@@ -8,6 +9,8 @@ use rand::random;
 
 const DEFAULT_ARRAY_SIZE: usize = 1000;
 const DATA_SIZE: usize = 20;
+
+const array_size_list: [usize; 7] = [10, 100, 1000, 2000, 4000, 8000, 16000];
 
 #[repr(C)]
 #[derive(Debug,Clone,Copy)]
@@ -28,46 +31,40 @@ fn make_str() -> [char; DATA_SIZE]
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let mut data: Vec<Record> = Vec::new();
-  let mut accum: u32 = 0;
 
-  let args: Vec<String> = args().collect();
+  for array_size in array_size_list {
 
-  let array_size =
-    if args.len() > 1
+    let mut data: Vec<Record> = Vec::new();
+    let mut accum: u32 = 0;
+
+    for _i in (Range { start: 0, end: array_size })
     {
-      args[1].parse::<usize>().unwrap()
-    } else {
-      DEFAULT_ARRAY_SIZE
-    };
+      accum += (random::<u32>() % 100) + 1;
+      data.push(Record {
+        key: accum,
+        value: make_str()
+      })
 
-  for _i in (Range { start: 0, end: array_size })
-  {
-    accum += (random::<u32>() % 100) + 1;
-    data.push(Record {
-      key: accum,
-      value: make_str()
-    })
+    }
 
-  }
+    let filename = format!("data_{}.dat", array_size);
+    let mut file = File::create(&filename)?;
 
-  let filename = format!("data_{}.dat", array_size);
-  let mut file = File::create(&filename)?;
+    println!("Generating {} records of size {} each = 0x{:x} -> {}", 
+        array_size,
+        size_of::<Record>(),
+        size_of::<Record>(),
+        &filename
+      );
 
-  println!("Generating {} records of size {} each = 0x{:x} -> {}", 
-      array_size,
-      size_of::<Record>(),
-      size_of::<Record>(),
-      &filename
-    );
+    for i in data
+    {
+      let buffer: [u8; size_of::<Record>()] = 
+        unsafe { transmute::<Record, [u8; size_of::<Record>()]>(i) };
 
-  for i in data
-  {
-    let buffer: [u8; size_of::<Record>()] = 
-      unsafe { transmute::<Record, [u8; size_of::<Record>()]>(i) };
+      file.write_all(&buffer)?;
+    }
 
-    // println!("{:?}", i);
-    file.write_all(&buffer)?;
   }
 
   Ok(())
--- p1_binary_search_solution/src/tests.rs
+++ p1_binary_search_solution/src/tests.rs
@@ -1,4 +1,6 @@
 use crate::data_file::{DataFile, Record};
+use std::{ops::Range, time::Instant};
+
 
 const TEST_FILE:&str = "test_data.dat";
 const TEST_FILE_SIZE: usize = 1000;
@@ -20,38 +22,58 @@ const FIRST_RECORD: Record = Record {
 };
 
 #[test]
-fn open_file()
-{
-  let file = DataFile::open(&TEST_FILE.to_string()).unwrap();
-  assert!(file.len() == TEST_FILE_SIZE);
-}
+fn written_2_test() {
 
-#[test]
-fn get_one()
-{
-  let mut file = DataFile::open(&TEST_FILE.to_string()).unwrap();
-  let result = file.get(file.len() / 2).unwrap();
+  let test_size = 100000;
+  let data_info = vec![
+    ("data_10.dat", 10),
+    ("data_100.dat", 100),
+    ("data_1000.dat", 1000),
+    ("data_2000.dat", 2000),
+    ("data_4000.dat", 4000),
+    ("data_8000.dat", 8000),
+    ("data_16000.dat", 16000)
+    ];
 
-  println!("get({}): {:?}", file.len() / 2, result);
-  assert!(result == MID_RECORD);
-}
+  for (test_file, array_size) in data_info {
 
-#[test]
-fn find_present()
-{
-  let mut file = DataFile::open(&TEST_FILE.to_string()).unwrap();
-  let result = file.find(FOUND_RECORD.key).unwrap().unwrap();
+    let mut time_list: Vec<f32> = Vec::new();
 
-  println!("find({}): {:?}", FOUND_RECORD.key, result);
-  assert!(result == FOUND_RECORD);
-}
+    // heat system up
+    for _i in (Range { start: 0, end: test_size }) {
+      let mut file = DataFile::open(&TEST_FILE.to_string()).unwrap();
+      file.find(24979).unwrap().unwrap();
+    }
 
-#[test]
-fn find_missing()
-{
-  let mut file = DataFile::open(&TEST_FILE.to_string()).unwrap();
-  let result = file.find(FOUND_RECORD.key-1).unwrap().unwrap();
+    println!("Heating up done for {}", test_file);
 
-  println!("find({}): {:?}", FOUND_RECORD.key-1, result);
-  assert!(result == FOUND_RECORD);
+    let mut used_time: f32 = 0.0;
+    let mut file = DataFile::open(&test_file.to_string()).unwrap();
+
+    while used_time < 10.0 {
+
+      let key = rand::random::<u32>();
+      let start = Instant::now();
+      file.find(key).unwrap().unwrap();
+      let end = Instant::now();
+      let time = (end-start).as_secs_f32();
+      time_list.push(time);
+      used_time += time;
+
+    }
+
+    let total_time: f32 = time_list.iter().sum();
+    let mean_time = total_time / time_list.len() as f32;
+    let variance = time_list.iter().map(|value| {
+      let diff = mean_time - (*value as f32);
+      diff * diff
+    }).sum::<f32>() / test_size as f32;
+
+    println!("Experiment with {} elements", array_size);
+    println!("Total Time: {}", total_time);
+    println!("Average Time: {}", mean_time);
+    println!("Variance: {}\n\n", variance);
+    
+  }
+
 }
